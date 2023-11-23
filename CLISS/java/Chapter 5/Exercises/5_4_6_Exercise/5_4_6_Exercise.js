47aa6221d86b761ec6bd02c0c70a79dced274b89
cosed429 <cosed429@school.lu>
2023-11-23 08:21:27 +0100

    commit

--- CLISS/java/Chapter 5/Exercises/5_4_6_Exercise/5_4_6_Exercise.js
+++ CLISS/java/Chapter 5/Exercises/5_4_6_Exercise/5_4_6_Exercise.js
@@ -2,6 +2,8 @@
         document.getElementById("rollButton").addEventListener("click", rollDice);
         function rollDice(){
             //write here your code instructions
+            //Make it so the  dice  changes the image of the dice according to the random number. 
 
+        
         }
         
