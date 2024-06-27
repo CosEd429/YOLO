6d89ae3078d883a73849729f855088870e2b2a45
cosed429 <cosed429@school.lu>
2024-06-27 14:26:34 +0200

    commit

---
+++ Cliss Project/TodoList.js
@@ -0,0 +1,22 @@
+const tasks = [];
+function addTask(){
+    var taskInput = document.getElementById("Task")
+let task = taskInput.value;
+if(task){
+    tasks.push[task, false];
+    taskInput.value = ""
+    alert("Task added.")    
+}	
+else{
+    alert("Error.")
+}
+}
+
+function removeRow(){
+    task.splice();
+}
+
+
+function displayTask(){
+    const tasks
+}
